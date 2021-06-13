c7fd7a62ce4b5cc830a5258d3ef19c58451d5e31
Bolivar <berriobolivar@gmail.com>
2021-06-13 17:53:49 -0500

    Adding contact form

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,14 +3,14 @@ import './About.css'
 
 function About() {
     return (
-        <div id="about" className="about content-container">
+        <section id="about" className="about container">
             <h3 className="section-title">About me</h3>
             <p className="about-summary">
                 I am a software developer, with more than tree years of experience building web applications.
                 <br></br>
                 Self taught, proactive and always looking for new challenges.
             </p>
-        </div>
+        </section>
     );
 }
 
--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import About from '../About/About';
 import Experience from '../Experience/Experience';
-
+import Contact from '../Contact/Contact';
 function Content() {
     return (
         <div className="content">
             <About />
             <Experience />
+            <Contact />
         </div>
     );
 }
