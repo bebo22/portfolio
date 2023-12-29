05f1cfc070995209abd16b065e5aeca8bd3f683a
Bolivar Berrio <berriobolivar@gmail.com>
2023-12-28 20:49:09 -0500

    Hero initial theming

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <Header />
   <main>
+    <Hero />
   </main>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import Header from './components/layouts/Header/Header.vue';
+import Hero from '@components/layouts/hero/Hero.vue';
 
 export default defineComponent({
   components: {
     Header,
+    Hero,
   },
 });
 </script>
---
+++ src/components/layouts/hero/Hero.vue
@@ -0,0 +1,20 @@
+<script>
+import './hero.css';
+</script>
+<template>
+  <div class="hero">
+    <section class="hero__inner container">
+      <article class="hero__info">
+        <img src="@assets/profile-picture.png" alt="Hero profile picture" loading="lazy" width="177" height="177" />
+        <section>
+          <h1>Welcome</h1>
+          <p class="subheading">Description about me Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+            commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient </p>
+        </section>
+      </article>
+      <aside class="hero__image">
+        <img src="@assets/hero-image.png" alt="Hero astronaut image" loading="lazy" width="422" height="440" />
+      </aside>
+    </section>
+  </div>
+</template>
